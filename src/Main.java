f2d1e7b6e670cc670919da1ad36755b98aaae25b
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-10 19:24:15 -0300

    Leitura dos dados

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N, posicao, maior;
+		
+		for(posicao = 0; posicao <= 10; posicao ++) {
+			N = sc.nextInt();
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
