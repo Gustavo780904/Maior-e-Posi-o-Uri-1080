ec1e3f6a5f1d945d7a5c58bbb3a4ee7a4a144489
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-10 19:43:34 -0300

    Projeto finalisado

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,17 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int N, posicao, maior;
+		int N = 0, ordem = 0, posicao, maior = 0;
 		
-		for(posicao = 0; posicao <= 10; posicao ++) {
+		for(posicao = 1; posicao <= 100; posicao ++) {
 			N = sc.nextInt();
+			if (N > maior) {
+				maior = N;
+				ordem = posicao;
+			}
 		}
-		
+		System.out.println(maior);
+		System.out.println(ordem);
 		
 		
 		sc.close();
